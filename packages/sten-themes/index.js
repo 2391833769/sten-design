a009568487dbc7d14215d6b4cfd6354e387cb7bc
bianhuicong <bianhuicong@58.com>
2025-06-23 20:08:37 +0800

    123

--- packages/sten-themes/index.js
+++ packages/sten-themes/index.js
@@ -1,5 +1,16 @@
 const testTheme = {
     "editor.background": "#fff",
+    "editor.lineHighlightBackground": "#fff",
+    "editorLineNumber.foreground": "#000",
+    "editorLineNumber.activeForeground": "#000",
+    "editor.selectionBackground": "#fff",
+    "editor.selectionHighlightBackground": "#fff",
+    "editor.wordHighlightBackground": "#fff",
+    "editor.wordHighlightStrongBackground": "#fff",
+    "editorCursor.foreground": "#000",
+    "editorIndentGuide.background": "#000",
+    "editorIndentGuide.activeBackground": "#000",
+    "editorRuler.foreground": "#000",
 }
 
 
